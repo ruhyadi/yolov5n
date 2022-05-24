42d105dd8f3aba24dea808c2693bb89c8c1c51fa
ruhyadi <ruhyadi.dr@gmail.com>
2022-05-24 16:26:31 +0700

    version 2.0: fix hubconf

--- hubconf.py
+++ hubconf.py
@@ -1,138 +1,53 @@
-# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
-"""
-PyTorch Hub models https://pytorch.org/hub/ultralytics_yolov5/
-
-Usage:
-    import torch
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-    model = torch.hub.load('ultralytics/yolov5:master', 'custom', 'path/to/yolov5s.onnx')  # file from branch
-"""
-
+import os
 import torch
+import git
+from pathlib import Path
+from models.common import AutoShape
 
+from models.yolo import Model
 
-def _create(name, pretrained=True, channels=3, classes=80, autoshape=True, verbose=True, device=None):
-    """Creates or loads a YOLOv5 model
+def _create(name: str = None, tag: str = None, classes: int = None, device: str = 'cpu'):
 
-    Arguments:
-        name (str): model name 'yolov5s' or path 'path/to/best.pt'
-        pretrained (bool): load pretrained weights into the model
-        channels (int): number of input channels
-        classes (int): number of model classes
-        autoshape (bool): apply YOLOv5 .autoshape() wrapper to model
-        verbose (bool): print all information to screen
-        device (str, torch.device, None): device to use for model parameters
+    # TODO: add your own weights list with extension
+    weights_list = ['yolov5_nodeflux.pt']
+    idx = [x.split('.')[0] for x in weights_list].index(name)
+    assert name in [x.split('.')[0] for x in weights_list], \
+        f'[INFO] not found, try: {weights_list}'
 
-    Returns:
-        YOLOv5 model
-    """
-    from pathlib import Path
+    repo = git.Repo(os.getcwd())
+    repo_url = Path(repo.remotes.origin.url)
+    repo_tag = tag
+    if tag is None:
+        repo_tag = repo.tags[-1] # last tag
+    weights_url = repo_url / "releases/download" / str(repo_tag) / str(weights_list[idx])
+    # https:/github.com/username/repo/releases/download/tags/model.pt
 
-    from models.common import AutoShape, DetectMultiBackend
-    from models.yolo import Model
-    from utils.downloads import attempt_download
-    from utils.general import LOGGER, check_requirements, intersect_dicts, logging
-    from utils.torch_utils import select_device
+    # TODO: load your own model configuration
+    cfg = list((Path(__file__).parent / 'models').rglob(f'{name}.yaml'))[0]  # model.yaml path
+    model = Model(cfg, nc=classes)
+    model.load_state_dict(
+        torch.hub.load_state_dict_from_url(weights_url, map_location=device)['model']
+        )
+    model = AutoShape(model)
 
-    if not verbose:
-        LOGGER.setLevel(logging.WARNING)
-    check_requirements(exclude=('tensorboard', 'thop', 'opencv-python'))
-    name = Path(name)
-    path = name.with_suffix('.pt') if name.suffix == '' else name  # checkpoint path
-    try:
-        device = select_device(('0' if torch.cuda.is_available() else 'cpu') if device is None else device)
+    return model.to(device)
 
-        if pretrained and channels == 3 and classes == 80:
-            model = DetectMultiBackend(path, device=device)  # download/load FP32 model
-            # model = models.experimental.attempt_load(path, map_location=device)  # download/load FP32 model
-        else:
-            cfg = list((Path(__file__).parent / 'models').rglob(f'{path.stem}.yaml'))[0]  # model.yaml path
-            model = Model(cfg, channels, classes)  # create model
-            if pretrained:
-                ckpt = torch.load(attempt_download(path, repo='ruhyadi/yolov5n'), map_location=device)  # load
-                csd = ckpt['model'].float().state_dict()  # checkpoint state_dict as FP32
-                csd = intersect_dicts(csd, model.state_dict(), exclude=['anchors'])  # intersect
-                model.load_state_dict(csd, strict=False)  # load
-                if len(ckpt['model'].names) == classes:
-                    model.names = ckpt['model'].names  # set class names attribute
-        if autoshape:
-            model = AutoShape(model)  # for file/URI/PIL/cv2/np inputs and NMS
-        return model.to(device)
-
-    except Exception as e:
-        help_url = 'https://github.com/ultralytics/yolov5/issues/36'
-        s = f'{e}. Cache may be out of date, try `force_reload=True` or see {help_url} for help.'
-        raise Exception(s) from e
-
-
-def custom(path='path/to/model.pt', autoshape=True, verbose=True, device=None):
-    # YOLOv5 custom or local model
-    return _create(path, autoshape=autoshape, verbose=verbose, device=device)
-
-
-def yolov5_nodeflux(pretrained=True, channels=3, classes=5, autoshape=True, verbose=True, device=None):
-    # YOLOv5-Nodeflux
-    return _create('yolov5_nodeflux', pretrained, channels, classes, autoshape, verbose, device)
-
-
-def yolov5n(pretrained=True, channels=3, classes=80, autoshape=True, verbose=True, device=None):
-    # YOLOv5-nano model https://github.com/ultralytics/yolov5
-    return _create('yolov5n', pretrained, channels, classes, autoshape, verbose, device)
-
-
-def yolov5s(pretrained=True, channels=3, classes=80, autoshape=True, verbose=True, device=None):
-    # YOLOv5-small model https://github.com/ultralytics/yolov5
-    return _create('yolov5s', pretrained, channels, classes, autoshape, verbose, device)
-
-
-def yolov5m(pretrained=True, channels=3, classes=80, autoshape=True, verbose=True, device=None):
-    # YOLOv5-medium model https://github.com/ultralytics/yolov5
-    return _create('yolov5m', pretrained, channels, classes, autoshape, verbose, device)
-
-
-def yolov5l(pretrained=True, channels=3, classes=80, autoshape=True, verbose=True, device=None):
-    # YOLOv5-large model https://github.com/ultralytics/yolov5
-    return _create('yolov5l', pretrained, channels, classes, autoshape, verbose, device)
-
-
-def yolov5x(pretrained=True, channels=3, classes=80, autoshape=True, verbose=True, device=None):
-    # YOLOv5-xlarge model https://github.com/ultralytics/yolov5
-    return _create('yolov5x', pretrained, channels, classes, autoshape, verbose, device)
-
-
-def yolov5n6(pretrained=True, channels=3, classes=80, autoshape=True, verbose=True, device=None):
-    # YOLOv5-nano-P6 model https://github.com/ultralytics/yolov5
-    return _create('yolov5n6', pretrained, channels, classes, autoshape, verbose, device)
-
-
-def yolov5s6(pretrained=True, channels=3, classes=80, autoshape=True, verbose=True, device=None):
-    # YOLOv5-small-P6 model https://github.com/ultralytics/yolov5
-    return _create('yolov5s6', pretrained, channels, classes, autoshape, verbose, device)
-
-
-def yolov5m6(pretrained=True, channels=3, classes=80, autoshape=True, verbose=True, device=None):
-    # YOLOv5-medium-P6 model https://github.com/ultralytics/yolov5
-    return _create('yolov5m6', pretrained, channels, classes, autoshape, verbose, device)
-
-
-def yolov5l6(pretrained=True, channels=3, classes=80, autoshape=True, verbose=True, device=None):
-    # YOLOv5-large-P6 model https://github.com/ultralytics/yolov5
-    return _create('yolov5l6', pretrained, channels, classes, autoshape, verbose, device)
-
-
-def yolov5x6(pretrained=True, channels=3, classes=80, autoshape=True, verbose=True, device=None):
-    # YOLOv5-xlarge-P6 model https://github.com/ultralytics/yolov5
-    return _create('yolov5x6', pretrained, channels, classes, autoshape, verbose, device)
-
-
+# TODO: define your model name
+def yolov5_nodeflux(imgsize=640, classes=5, device='cpu'):
+    # load custom model
+    return _create('yolov5_nodeflux', tag='v2.0', device=device)
+    
 if __name__ == '__main__':
-    # model = _create(name='yolov5s', pretrained=True, channels=3, classes=80, autoshape=True, verbose=True)  # pretrained
-    # model = custom(path='path/to/model.pt')  # custom
 
-    model = torch.hub.load('ruhyadi/yolov5n:cvat', 'yolov5_nodeflux')
+    import argparse
 
-    imgs = ['data/images/bus.jpg']
+    parser = argparse.ArgumentParser(description="Torch Hub Testing")
+    parser.add_argument('--repository', type=str, required=True, help='Repository, username/reponame')
+    parser.add_argument('--tag', type=str, required=True, help='Release tag')
+    args = parser.parse_args()
 
-    results = model(imgs, size=640)
-    json = results.pandas().xyxy[0].to_dict('records')
-    print(json)
+    weights = torch.hub.list(f'{args.repository}:{args.tag}', force_reload=True)
+
+    for weight in weights:
+        model = torch.hub.load(f'{args.repository}:{args.tag}', weight, tag={args.tag})
+        print(f'[INFO] Success load {weight.upper()}')
