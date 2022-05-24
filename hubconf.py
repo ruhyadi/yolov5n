418009c8c46b037b4bdb407ffab7fdb4115114d8
ruhyadi <ruhyadi.dr@gmail.com>
2022-05-24 19:03:50 +0700

    change hubconf to origin

--- hubconf.py
+++ hubconf.py
@@ -1,50 +1,74 @@
-import os
+# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
+"""
+PyTorch Hub models https://pytorch.org/hub/ultralytics_yolov5/
+
+Usage:
+    import torch
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    model = torch.hub.load('ultralytics/yolov5:master', 'custom', 'path/to/yolov5s.onnx')  # file from branch
+"""
+
 import torch
-import git
 
-def _create(name: str = None, tag: str = None, classes: int = None, device: str = 'cpu'):
 
+def _create(name, pretrained=True, channels=3, classes=80, autoshape=True, verbose=True, device=None):
+    """Creates or loads a YOLOv5 model
+
+    Arguments:
+        name (str): model name 'yolov5s' or path 'path/to/best.pt'
+        pretrained (bool): load pretrained weights into the model
+        channels (int): number of input channels
+        classes (int): number of model classes
+        autoshape (bool): apply YOLOv5 .autoshape() wrapper to model
+        verbose (bool): print all information to screen
+        device (str, torch.device, None): device to use for model parameters
+
+    Returns:
+        YOLOv5 model
+    """
     from pathlib import Path
-    from models.common import AutoShape
+
+    from models.common import AutoShape, DetectMultiBackend
     from models.yolo import Model
-    from utils.general import intersect_dicts
+    from utils.downloads import attempt_download
+    from utils.general import LOGGER, check_requirements, intersect_dicts, logging
+    from utils.torch_utils import select_device
 
-    # TODO: add your own weights list with extension
-    weights_list = ['yolov5_nodeflux.pt']
-    idx = [x.split('.')[0] for x in weights_list].index(name)
-    assert name in [x.split('.')[0] for x in weights_list], \
-        f'[INFO] not found, try: {weights_list}'
+    if not verbose:
+        LOGGER.setLevel(logging.WARNING)
+    check_requirements(exclude=('tensorboard', 'thop', 'opencv-python'))
+    name = Path(name)
+    path = name.with_suffix('.pt') if name.suffix == '' else name  # checkpoint path
+    try:
+        device = select_device(('0' if torch.cuda.is_available() else 'cpu') if device is None else device)
 
-    repo = git.Repo(os.getcwd())
-    username = (repo.remotes.origin.url).split('/')[-2]
-    reponame = (repo.remotes.origin.url).split('/')[-1]
-    repotag = tag
-    if tag is None:
-        repotag = repo.tags[-1] # last tag
-    weights_url = f'https://github.com/{username}/{reponame}/releases/download/{repotag}/{weights_list[idx]}'    # https:/github.com/username/repo/releases/download/tags/model.pt
-    # https:/github.com/ruhyadi/yolov5n/releases/download/v2.2/yolov5_nodeflux.pt
+        if pretrained and channels == 3 and classes == 80:
+            model = DetectMultiBackend(path, device=device)  # download/load FP32 model
+            # model = models.experimental.attempt_load(path, map_location=device)  # download/load FP32 model
+        else:
+            cfg = list((Path(__file__).parent / 'models').rglob(f'{path.stem}.yaml'))[0]  # model.yaml path
+            model = Model(cfg, channels, classes)  # create model
+            if pretrained:
+                ckpt = torch.load(attempt_download(path, repo='ruhyadi/yolov5n'), map_location=device)  # load
+                csd = ckpt['model'].float().state_dict()  # checkpoint state_dict as FP32
+                csd = intersect_dicts(csd, model.state_dict(), exclude=['anchors'])  # intersect
+                model.load_state_dict(csd, strict=False)  # load
+                if len(ckpt['model'].names) == classes:
+                    model.names = ckpt['model'].names  # set class names attribute
+        if autoshape:
+            model = AutoShape(model)  # for file/URI/PIL/cv2/np inputs and NMS
+        return model.to(device)
 
-    # TODO: load your own model configuration
-    cfg = list((Path(__file__).parent / 'models').rglob(f'{name}.yaml'))[0]  # model.yaml path
-    model = Model(cfg, nc=classes)
-    
-    ckpt = torch.hub.load_state_dict_from_url(weights_url, map_location=device)
-    csd = ckpt['model'].float().state_dict()
-    csd = intersect_dicts(csd, model.state_dict(), exclude=['anchors'])
-    model.load_state_dict(csd, strict=False)
+    except Exception as e:
+        help_url = 'https://github.com/ultralytics/yolov5/issues/36'
+        s = f'{e}. Cache may be out of date, try `force_reload=True` or see {help_url} for help.'
+        raise Exception(s) from e
 
-    if len(ckpt['model'].names) == classes:
-        model.names = ckpt['model'].names  # set class names attribute
-    
-    model = AutoShape(model)
+def yolov5_nodeflux(pretrained=True, channels=3, classes=5, autoshape=True, verbose=True, device=None):
+    # YOLOv5-Nodeflux
+    return _create('yolov5_nodeflux', pretrained, channels, classes, autoshape, verbose, device)
 
-    return model.to(device)
 
-# TODO: define your model name
-def yolov5_nodeflux(imgsize=640, classes=5, device='cpu'):
-    # load custom model
-    return _create('yolov5_nodeflux', tag='v3.0', device=device)
-    
 if __name__ == '__main__':
 
     import argparse
